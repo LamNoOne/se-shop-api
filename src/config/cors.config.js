c1446b7dde82214ed02b5ad1ae4bc56c7408cdec
LamNoOne <trannguyendaclam@gmail.com>
2024-05-01 15:44:31 +0700

    config-cors

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -7,23 +7,18 @@ const { getCorsDomainByDomain } = require('~/api/v1/repositories/cors.domain.rep
 
 const ApiError = require('~/core/api.error')
 
-// const corsOptions = {
-//   origin: async function (origin, callback) {
-//     if (!origin && nodeEnv === NODE_ENV_DEV) {
-//       return callback(null, true)
-//     }
-//     const corsDomain = await getCorsDomainByDomain(origin)
-//     if (corsDomain) {
-//       return callback(null, true)
-//     }
-//     return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy`))
-//   },
-//   optionsSuccessStatus: 200,
-//   credentials: true
-// }
-
 const corsOptions = {
-  origin: '*',
+  origin: async function (origin, callback) {
+    // if (!origin && nodeEnv === NODE_ENV_DEV) {
+    //   return callback(null, true)
+    // }
+    // const corsDomain = await getCorsDomainByDomain(origin)
+    // if (corsDomain) {
+    //   return callback(null, true)
+    // }
+    // return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy`))
+    return callback(null, true)
+  },
   optionsSuccessStatus: 200,
   credentials: true
 }
